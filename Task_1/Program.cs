cf947740ede363183e2afff19b7805d2c750432f
Vladislav <vlad.davidovich.2014@mail.ru>
2022-08-07 21:41:21 +0300

    Программа где на вход принимается 2 числа и выводится какое бельшее, какое меньшее

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} = болшее, {num2} = меньшее");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine($"{num2} = болшее, {num1} = меньшее");
+}
