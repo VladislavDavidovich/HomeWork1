63e9de39a0aecbe2d886cdb9775c1e1883044d99
Vladislav <vlad.davidovich.2014@mail.ru>
2022-08-10 14:17:16 +0300

    Программа на поиск максимального из 3-х чисел

---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if(num1 > num2) 
+{
+   max = num1;
+}   
+
+else if(num2 > num3) 
+{
+    max = num2;
+}
+
+else if(num3 > num1) 
+{
+    max = num3;
+}
+{
+    Console.WriteLine(max);
+}
+ 
