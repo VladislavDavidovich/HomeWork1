b99729c4e1ce349e06ed82c1bfd9adfa3188be7e
Vladislav <vlad.davidovich.2014@mail.ru>
2022-08-10 16:34:56 +0300

    программа на нахождение всех четных чисел

---
+++ Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool onenumber = number % 2 == 1;
+
+while(number < 1);
+{
+    Console.WriteLine(number);
+    number = number - 1;
+}
